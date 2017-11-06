2f2566563db3cd9a8f7f2bf407ae73cdb462d31f
Nisarg Patel <nisarg1792@gmail.com>
2017-11-06 16:50:31 -0500

    Updated submit.php
    Review the changes

--- submit.php
+++ submit.php
@@ -1,12 +1,17 @@
 <?php 
-$name = $_POST['fname'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$phone = $_POST['phone'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "aditi.biniwale@yahoo.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+  $to = 'aditi.biniwale@yahoo.com' ;  // Replace email address with your own
+  $subject = 'Contact Form' ; 
+  $name = $_POST['fname'] ; 
+  $email = $_POST['email'] ;
+  $message = $_POST['message'] ;
+  $phone = $_POST['phone'] ;
+
+  $message = "From: $fname \nEmail: $email \nMessage: $message \nPhone: $phone \n";
+
+  $sent = mail($to, $subject, $message) ; 
+    if($sent) {
+      echo "<h3>Thank you for your message.</h3><p>Your email has been sent successfully and we appreciate you getting in touch with us. We will be sending a reply soon.</p>"; 
+    }else{
+      echo "<h3>Sorry, your message wasn't sent.</h3><p>We seem to have encountered a problem sending your message. Please go back and try again. If you get this message again please email <a href='mailto:webmaster@yourDomain.com'>webmaster@yourDomain.com</a>.</p>"; 
+}
 ?>
